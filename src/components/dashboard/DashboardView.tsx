3318a95fec07287a1f569b05d0d48f431e891a1c
mersad <mersad_998@yahoo.com>
2022-10-05 14:44:48 +0330

    feat: dashboard page

---
+++ src/components/dashboard/DashboardView.tsx
@@ -0,0 +1,20 @@
+import { useTheme } from '@mui/material/styles';
+
+import dashboardImage from '../../images/heo-fun.gif';
+import { FC } from 'react';
+
+import type { DashboardViewProps } from './DashboardTypes';
+
+const DashboardView: FC<DashboardViewProps> = (props) => {
+  const theme = useTheme();
+  console.log('theme: ', props, theme);
+
+  return (
+    <div>
+      <img src={dashboardImage} alt="" height={300} width={400} />
+      dashboard
+    </div>
+  );
+};
+
+export default DashboardView;
