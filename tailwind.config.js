2365caf81a6ed3070fb6bac38b58800193568998
mersad <mersad_998@yahoo.com>
2022-10-05 16:06:10 +0330

    add styled components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily:{
-        'Dyna': 'dyna'
-      }
+      fontFamily: {
+        Dyna: 'dyna',
+      },
     },
   },
   plugins: [],
-}
+};
