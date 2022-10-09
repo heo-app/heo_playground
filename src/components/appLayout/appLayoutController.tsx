50d4d851807e5fbd4eec6cb8e1d9a17e5679bc84
mersad <mersad_998@yahoo.com>
2022-10-09 16:49:36 +0330

    feat: pass toggle drawer to header with forwardRef

--- src/components/appLayout/appLayoutController.tsx
+++ src/components/appLayout/appLayoutController.tsx
@@ -1,12 +1,27 @@
-import { FC, memo } from 'react';
-import type { AppLayoutControllerProps } from './appLayoutTypes';
+import { FC, ForwardedRef, memo, useRef } from 'react';
 
-import AppLayoutView from './appLayoutView';
+import AppLayoutView from './AppLayoutView';
+
+import type { AppLayoutControllerProps } from './AppLayoutTypes';
+import type { ToggleDrawer } from '../drawer/DrawerTypes';
 
 const appLayoutController: FC<AppLayoutControllerProps> = (props) => {
   const { children } = props;
 
-  return <AppLayoutView children={children} />;
+  const drawerRef = useRef<{ toggleDrawer: ToggleDrawer }>();
+
+  const onHamburgerButtonClick: ToggleDrawer = (): void => {
+    // here has been used optional chaining because of first render delay and it doesn't effect in user experience
+    drawerRef.current?.toggleDrawer();
+  };
+
+  return (
+    <AppLayoutView
+      children={children}
+      onHamburgerButtonClick={onHamburgerButtonClick}
+      ref={drawerRef as ForwardedRef<{ toggleDrawer: ToggleDrawer }>}
+    />
+  );
 };
 
 // true in the next line will cause to always memorize this component and render it only one time
--- src/components/appLayout/appLayoutTypes.ts
+++ src/components/appLayout/appLayoutTypes.ts
@@ -1,8 +1,11 @@
-import type { ReactElement } from 'react';
+import type { ForwardedRef, ReactElement } from 'react';
+import type { ToggleDrawer } from '../drawer/DrawerTypes';
 
 export interface AppLayoutControllerProps {
   children: ReactElement;
 }
 export interface AppLayoutViewProps {
+  onHamburgerButtonClick: ToggleDrawer;
   children: ReactElement;
+  ref?: ForwardedRef<{ toggleDrawer: ToggleDrawer }>;
 }
