1f1f3e48dda335e370d56f3456c4299f8a273e27
mersad <mersad_998@yahoo.com>
2022-11-01 16:54:13 +0330

    feat: add profile page to routs

--- src/chore/routes.tsx
+++ src/chore/routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { AppLayout } from '../components/appLayout';
 import { LoginPage } from '../components/login';
 import { DashboardPage } from '../components/dashboard';
+import { Profile } from 'src/components/profile';
 
 export const customRoutes = (
   <Routes>
@@ -14,6 +15,16 @@ export const customRoutes = (
         </AppLayout>
       }
     />
+
+    <Route
+      path="/profile"
+      element={
+        <AppLayout>
+          <Profile />
+        </AppLayout>
+      }
+    />
+
     <Route path="/login" element={<LoginPage />} />
   </Routes>
 );
