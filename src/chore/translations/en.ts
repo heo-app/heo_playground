e0af1cda81a457e4dcb0582e3e6558ce99071585
mersad <mersad_998@yahoo.com>
2022-11-13 17:34:15 +0330

    add En dictionary

---
+++ src/chore/translations/en.ts
@@ -0,0 +1,31 @@
+export const EnTranslations = {
+  menu: {
+    home: 'home',
+    profile: 'profile',
+  },
+  login: {
+    welcomeMessage: 'Welcome to heo app',
+    appSlogan: 'lets make a better life',
+    createAccount: 'Create Account!',
+    register: 'Register',
+    welcomeBack: 'welcome back',
+    mainJob: 'we are here to help each other',
+    repetition: 'everyday - everywhere',
+    username: 'UserName',
+    password: 'PassWord',
+    forgetPassword: 'Forget PassWord',
+    startNow: 'Start Now!',
+    signInWithGoogle: 'Sign in with google',
+  },
+  post: {
+    interested: 'Im interested',
+  },
+  profile: {
+    editProfile: 'Edit Profile',
+    firstName: 'first name',
+    lastName: 'last name',
+    mobile: 'mobile number',
+    email: 'E-mail',
+    bio: 'bio',
+  },
+};
