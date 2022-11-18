71e494c7ecb1c0d13a66970b50d2b94d7a4356b2
mersad <mersad_998@yahoo.com>
2022-11-18 17:34:45 +0330

    add Ar dictionary

---
+++ src/chore/translations/fa.ts
@@ -0,0 +1,31 @@
+export const FaTranslations = {
+  menu: {
+    home: 'خانه',
+    profile: 'مشخصات',
+  },
+  login: {
+    welcomeMessage: 'خوش آمدید',
+    appSlogan: 'بیا زندگی بهتری بسازیم',
+    createAccount: 'ایجاد حساب کاربری!',
+    register: 'ثبت نام',
+    welcomeBack: 'خوش آمدید',
+    mainJob: 'ما اینجا هستیم تا به هم کمک کنیم',
+    repetition: 'هر روز - همه جا',
+    username: 'نام کاربری',
+    password: 'کلمه عبور',
+    forgetPassword: 'فراموشی رمز عبور',
+    startNow: 'الان شروع کن!',
+    signInWithGoogle: 'با گوگل وارد شوید',
+  },
+  post: {
+    interested: 'علاقه مندم',
+  },
+  profile: {
+    editProfile: 'ویرایش نمایه',
+    firstName: 'نام کوچک',
+    lastName: 'نام خانوادگی',
+    mobile: 'شماره موبایل',
+    email: 'پست الکترونیک',
+    bio: 'بیو',
+  },
+};
