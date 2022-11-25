3a805cbb12dcb1ef916b88013b91765e87dd8d10
mersad <mersad_998@yahoo.com>
2022-11-25 23:22:31 +0330

    refactor: dashboard page

--- src/components/dashboard/DashboardView.tsx
+++ src/components/dashboard/DashboardView.tsx
@@ -1,15 +1,12 @@
 import { FC } from 'react';
-import { useTheme } from '@mui/material/styles';
 
-
-import type { DashboardViewProps } from './DashboardTypes';
 import { Posts } from '../posts';
 
-const DashboardView: FC<DashboardViewProps> = (props) => {
+import type { DashboardViewProps } from './DashboardTypes';
 
+const DashboardView: FC<DashboardViewProps> = () => {
   return (
     <div style={{ display: 'flex', flex: 1 }}>
-      {/* <img src={dashboardImage} alt="" height={300} width={400} /> */}
       <Posts />
     </div>
   );
