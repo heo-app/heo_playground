a4ba995b8d510599ae1a5cdd72e52f5050c9e6ab
mersad <mersad_998@yahoo.com>
2022-12-07 12:54:57 +0330

    fix translation problem

--- src/chore/translations/ar.ts
+++ src/chore/translations/ar.ts
@@ -1,31 +1,33 @@
 export const ArTranslations = {
-  menu: {
-    home: 'الصفحة الرئيسية',
-    profile: 'الملف الشخصي',
-  },
-  login: {
-    welcomeMessage: 'مرحبًا بك في تطبيق هيو',
-    appSlogan: 'لنجعل حياة أفضل',
-    createAccount: 'إصنع حساب!',
-    register: 'يسجل',
-    welcomeBack: 'مرحبًا بعودتك',
-    mainJob: 'نحن هنا لمساعدة بعضنا الاخر',
-    repetition: 'كل يوم - في كل مكان',
-    username: 'اسم االمستخدم',
-    password: 'كلمه السر',
-    forgetPassword: 'نسيت كلمة المرور',
-    startNow: 'ابدأ الآن!',
-    signInWithGoogle: 'الدخول مع جوجل',
-  },
-  post: {
-    interested: 'انا مهتم',
-  },
-  profile: {
-    editProfile: 'تعديل الملف الشخصي',
-    firstName: 'الاسم الاول',
-    lastName: 'اللقب',
-    mobile: 'رقم الهاتف المحمول',
-    email: 'البريد الإلكتروني',
-    bio: 'السيرة الذاتية',
+  translation: {
+    menu: {
+      home: 'الصفحة الرئيسية',
+      profile: 'الملف الشخصي',
+    },
+    login: {
+      welcomeMessage: 'مرحبًا بك في تطبيق هيو',
+      appSlogan: 'لنجعل حياة أفضل',
+      createAccount: 'إصنع حساب!',
+      register: 'يسجل',
+      welcomeBack: 'مرحبًا بعودتك',
+      mainJob: 'نحن هنا لمساعدة بعضنا الاخر',
+      repetition: 'كل يوم - في كل مكان',
+      username: 'اسم االمستخدم',
+      password: 'كلمه السر',
+      forgetPassword: 'نسيت كلمة المرور',
+      startNow: 'ابدأ الآن!',
+      signInWithGoogle: 'الدخول مع جوجل',
+    },
+    post: {
+      interested: 'انا مهتم',
+    },
+    profile: {
+      editProfile: 'تعديل الملف الشخصي',
+      firstName: 'الاسم الاول',
+      lastName: 'اللقب',
+      mobile: 'رقم الهاتف المحمول',
+      email: 'البريد الإلكتروني',
+      bio: 'السيرة الذاتية',
+    },
   },
 };
--- src/chore/translations/en.ts
+++ src/chore/translations/en.ts
@@ -1,31 +1,33 @@
 export const EnTranslations = {
-  menu: {
-    home: 'home',
-    profile: 'profile',
-  },
-  login: {
-    welcomeMessage: 'Welcome to heo app',
-    appSlogan: 'lets make a better life',
-    createAccount: 'Create Account!',
-    register: 'Register',
-    welcomeBack: 'welcome back',
-    mainJob: 'we are here to help each other',
-    repetition: 'everyday - everywhere',
-    username: 'UserName',
-    password: 'PassWord',
-    forgetPassword: 'Forget PassWord',
-    startNow: 'Start Now!',
-    signInWithGoogle: 'Sign in with google',
-  },
-  post: {
-    interested: 'Im interested',
-  },
-  profile: {
-    editProfile: 'Edit Profile',
-    firstName: 'first name',
-    lastName: 'last name',
-    mobile: 'mobile number',
-    email: 'E-mail',
-    bio: 'bio',
+  translation: {
+    menu: {
+      home: 'home',
+      profile: 'profile',
+    },
+    login: {
+      welcomeMessage: 'Welcome to heo app',
+      appSlogan: 'lets make a better life',
+      createAccount: 'Create Account!',
+      register: 'Register',
+      welcomeBack: 'welcome back',
+      mainJob: 'we are here to help each other',
+      repetition: 'everyday - everywhere',
+      username: 'UserName',
+      password: 'PassWord',
+      forgetPassword: 'Forget PassWord',
+      startNow: 'Start Now!',
+      signInWithGoogle: 'Sign in with google',
+    },
+    post: {
+      interested: 'Im interested',
+    },
+    profile: {
+      editProfile: 'Edit Profile',
+      firstName: 'first name',
+      lastName: 'last name',
+      mobile: 'mobile number',
+      email: 'E-mail',
+      bio: 'bio',
+    },
   },
 };
